58ced0136f84ec8800a7ccc78d47df3ae3a202c2
aseldom <a.seldom@gmail.com>
2023-04-07 08:57:36 +0600

    Утверждения с исключениями [#504886]

--- src/test/java/ru/job4j/assertj/NameLoadTest.java
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -41,8 +41,9 @@ class NameLoadTest {
                 .hasMessageContaining("this name:")
                 .hasMessageContaining(names[0])
                 .hasMessageContaining("does not contain a key");
-    }@Test
+    }
 
+    @Test
     void checkNameContainsValue() {
         NameLoad nameLoad = new NameLoad();
         String[] names = {"s="};
