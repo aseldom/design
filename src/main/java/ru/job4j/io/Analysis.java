897e22bab7cf448178ba3c3cbcf44365ca9c3922
aseldom <a.seldom@gmail.com>
2023-04-25 10:24:49 +0600

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+                PrintWriter out = new PrintWriter(new FileWriter(target))) {
+            String begin = "";
+            String end = "";
+            String s = "";
+            boolean find = false;
+            while ((s = in.readLine()) != null) {
+                String[] sar = s.split(" ");
+                if ("400".equals(sar[0]) || "500".equals(sar[0])) {
+                    if (!find) {
+                        begin = sar[1];
+                        find = true;
+                    }
+                } else if (find) {
+                    end = sar[1];
+                    find = false;
+                    out.printf("%s;%s%n", begin, end);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
