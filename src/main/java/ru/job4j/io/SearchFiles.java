7ac95520761929f2cae08faa48bc9b74a2b75ef2
aseldom <a.seldom@gmail.com>
2023-04-27 10:12:00 +0600

    10. Сканирование файловой системы [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    Predicate<Path> condition;
+    List<Path> list = new ArrayList<>();
+
+    SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    public List<Path> getPaths() {
+        return list;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (condition.test(file)) {
+            list.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
