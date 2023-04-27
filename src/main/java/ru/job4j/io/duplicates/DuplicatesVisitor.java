11bb1a328344062a463a12d88a3854ff64c1651b
aseldom <a.seldom@gmail.com>
2023-04-27 20:31:54 +0600

    11. Поиск дубликатов [#315066 #412645]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -6,13 +6,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-     private final HashMap<FileProperty, List<String>> store = new HashMap<>();
+     private final Map<FileProperty, List<String>> store = new HashMap<>();
 
      public void print() {
          for (FileProperty k : store.keySet()) {
@@ -27,10 +24,10 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (Files.isRegularFile(file)) {
             FileProperty newFile = new FileProperty(file.toFile().length(), file.getFileName().toString());
-            if (store.containsKey(newFile)) {
-                store.get(newFile).add(file.toAbsolutePath().toString());
-            } else {
-                store.put(newFile, new ArrayList<>(Arrays.asList(file.toAbsolutePath().toString())));
+            List<String> l = store.putIfAbsent(newFile,
+                    new ArrayList<>(Arrays.asList(file.toAbsolutePath().toString())));
+            if (l != null) {
+                l.add(file.toAbsolutePath().toString());
             }
         }
         return super.visitFile(file, attrs);
