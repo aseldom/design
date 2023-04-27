c1c521b4e40c8ae6e2f0cbb79bf9ed7d6d4ea8f3
aseldom <a.seldom@gmail.com>
2023-04-27 18:00:13 +0600

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,22 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        valid(args);
+        String f = args[0];
+        String extension = args[1];
+        Path start = Paths.get(f);
+        search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void valid(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Arguments length not equal 2");
+        }
     }
 }
