d40209df3d9c797f968f4f6c42b2cc098f96960d
aseldom <a.seldom@gmail.com>
2023-04-29 15:35:30 +0600

    14. Архивировать проект [#861 #412876]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Predicate;
@@ -9,11 +10,11 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File file : sources) {
-                zip.putNextEntry(new ZipEntry(file.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+            for (Path file : sources) {
+                zip.putNextEntry(new ZipEntry(file.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file.toString()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -33,15 +34,30 @@ public class Zip {
         }
     }
 
+    private void validateArgs(Path source, String extension, String target) {
+        if (!Files.isDirectory(source)) {
+            throw new IllegalArgumentException(String.format(
+                    "The directory - '%s' does not exist.", source));
+        }
+        if (!extension.startsWith(".") || !(extension.length() > 1)) {
+            throw new IllegalArgumentException(String.format(
+                    "The extension of file - '%s' does not correct.", extension));
+        }
+        if (target.length() < 5 || !target.endsWith(".zip")) {
+            throw new IllegalArgumentException(String.format(
+                    "The name of zip file - '%s' does not correct.", target));
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
         ArgsName argsName = ArgsName.of(args);
         Path source = Path.of(argsName.get("d"));
         String extension = argsName.get("e");
         File target = new File(argsName.get("o"));
-        argsName.pathValidate(source);
+        zip.validateArgs(source, extension, target.toString());
         Predicate<Path> predicate = p -> !p.toFile().getName().endsWith(extension);
-        List<File> listFiles = Search.search(source, predicate).stream().map(Path::toFile).toList();
+        List<Path> listFiles = Search.search(source, predicate);
         zip.packFiles(listFiles, target);
 
         zip.packSingleFile(
