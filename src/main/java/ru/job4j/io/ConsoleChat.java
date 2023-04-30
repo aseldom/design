8b6e6f3d870320eb80c22bd16055cee17bd6b652
aseldom <a.seldom@gmail.com>
2023-04-30 10:09:51 +0600

    16. Кодировка [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,78 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        List<String> log = new ArrayList<>();
+        List<String> answers  = readPhrases();
+        Random random = new Random();
+        int size = answers.size();
+        boolean run = true;
+        boolean stop = false;
+        while (run) {
+            System.out.println("Введите фразу:");
+            String in = scanner.nextLine();
+            log.add("Ввод пользователя: " + in);
+            if (in.equals(STOP) && !stop) {
+                stop = true;
+                log.add("Чат приостановлен");
+                System.out.println("Чат приостановлен, для продолжения введите слово: \"продолжить\"");
+            } else if (in.equals(CONTINUE) && stop) {
+                stop = false;
+                log.add("Чат продолжен");
+                System.out.println("Чат продолжен");
+            } else if (in.equals(OUT)) {
+                run = false;
+                log.add("Чат завершен");
+                System.out.println("Чат завершен");
+            } else if (!stop) {
+                String ans = answers.get(random.nextInt(size));
+                log.add("Ответ бота: " + ans);
+                System.out.println("Ответ бота: " + ans);
+            }
+        }
+        saveLog(log);
+    }
+
+    private List<String> readPhrases() {
+        List<String> res = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(botAnswers, StandardCharsets.UTF_8))) {
+            res =  reader.lines().toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    private void saveLog(List<String> log) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, StandardCharsets.UTF_8))) {
+            log.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("./data/logBot.txt", "./data/botAnswers.txt");
+        cc.run();
+    }
+}
