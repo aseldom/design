dc6a21fe9b44bab290b028c51e95c6ffc79f496b
aseldom <a.seldom@gmail.com>
2023-05-15 12:58:21 +0600

    3. Статистика по коллекции. [#45889 #411340]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,8 +1,6 @@
 package ru.job4j.question;
 
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Analize {
 
@@ -10,17 +8,25 @@ public class Analize {
         int added = 0;
         int changed = 0;
         int deleted = 0;
-        int equals = 0;
-        Set<User> dc = new HashSet<>(previous);
-        Set<User> nc = new HashSet<>(current);
-        dc.removeAll(current);
-        nc.removeAll(previous);
-        equals = previous.size() - dc.size();
-        dc.retainAll(nc);
-        changed = dc.size();
-        added = current.size() - equals - changed;
-        deleted = previous.size() - equals - changed;
-
+        Map<User, User> in = new HashMap<>();
+        Map<User, User> out = new HashMap<>();
+        current.forEach(s -> out.put(s, s));
+        previous.forEach(s -> in.put(s, s));
+        User user;
+        for (User inKey : in.keySet()) {
+            user = out.get(inKey);
+            if (user != null) {
+                if (user.getName().equals(inKey.getName())) {
+                    out.remove(inKey);
+                } else {
+                    changed++;
+                    out.remove(inKey);
+                }
+            } else {
+                deleted++;
+            }
+        }
+        added = out.size();
         return new Info(added, changed, deleted);
     }
 }
