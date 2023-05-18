462244a8e4b704d51840f600c49cb020f9a96c2f
aseldom <a.seldom@gmail.com>
2023-05-18 19:14:06 +0600

    Delete db directory

--- db/script.sql
+++
@@ -1,7 +0,0 @@
-select * from students;
-insert into students(name, age, klass)values('Vasiliy Semenov', '14', '8A');
-select * from students;
-update students set name='Anatoliy Popov', age = '13';
-select * from students;
-delete from students;
-select * from students;
