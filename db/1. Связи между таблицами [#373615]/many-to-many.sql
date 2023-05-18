a548c7267f1dc8ec5a3f9874f145a4f3dd17951e
aseldom <a.seldom@gmail.com>
2023-05-18 19:16:48 +0600

    Настройка PostgreSQL

---
+++ db/1. Связи между таблицами [#373615]/many-to-many.sql
@@ -0,0 +1,30 @@
+create table man(
+	id serial primary key,
+	name varchar(255)
+);
+
+create table shop(
+	id serial primary key,
+	name varchar(255)
+);
+
+create table man_shop(
+	id serial primary key,
+	man_id int references man(id),
+	shop_id int references shop(id)
+);
+
+insert into man(name) values('Roman');
+insert into man(name) values('Egor');
+insert into man(name) values('Vasily');
+
+insert into shop(name) values('Pyatjorochka');
+insert into shop(name) values('Universam');
+insert into shop(name) values('U_doma');
+
+insert into man_shop(man_id, shop_id) values(1, 2);
+insert into man_shop(man_id, shop_id) values(1, 3);
+insert into man_shop(man_id, shop_id) values(2, 1);
+insert into man_shop(man_id, shop_id) values(2, 2);
+insert into man_shop(man_id, shop_id) values(2, 3);
+insert into man_shop(man_id, shop_id) values(3, 3);
---
+++ db/script.sql
@@ -0,0 +1,7 @@
+select * from students;
+insert into students(name, age, klass)values('Vasiliy Semenov', '14', '8A');
+select * from students;
+update students set name='Anatoliy Popov', age = '13';
+select * from students;
+delete from students;
+select * from students;
