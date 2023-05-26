86bb4aaf77d9407b35e599ccc9b5afb51640f9a9
aseldom <a.seldom@gmail.com>
2023-05-26 18:16:46 +0600

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,25 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config conf = new Config("data/app.properties");
+        conf.load();
+        String driver = conf.value("hibernate.connection.driver_class");
+        String url = conf.value("hibernate.connection.url");
+        String login = conf.value("hibernate.connection.username");
+        String password = conf.value("hibernate.connection.password");
+        Class.forName(driver);
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
